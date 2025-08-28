e0766a338a067b3ca270342af31848c9d0c219c9
bonnieflowers <flowersbonnie94@gmail.com>
2025-08-28 16:24:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = 'dev-key-change-in-production'
-    TELEGRAM_BOT_TOKEN = '7502498778:AAFasWE3M0no2YVgfYPngtnolQKlIZ0rf44'
-    TELEGRAM_CHAT_ID = '6604253131'
+    TELEGRAM_BOT_TOKEN = '8010926424:AAFlcdE_J0BJjB8j86sCHajbjeKriWgo'
+    TELEGRAM_CHAT_ID = '7217555825'
     DEFAULT_REDIRECT = 'https://www.google.com'
     
     SQLALCHEMY_DATABASE_URI = 'postgresql://bonnie_user:M3jjcnjJ6Gzfl40ZaMD9Qtgj35RYRLGz@dpg-d2o5b875r7bs73dcheh0-a.oregon-postgres.render.com/bonnie'
